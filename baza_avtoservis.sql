ba41bf38e9da5cd542f39c9bb0a8b187fb165247
Goodchild21 <Goodchild_21@mail.ru>
2024-06-24 12:26:32 +0300

    Add files via upload

---
+++ baza_avtoservis.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Сustumers --Клиенты
+(
+    id SERIAL PRIMARY KEY,
+    firstName TEXT NOT NULL,
+    middlename TEXT,
+    lastName TEXT NOT NULL,
+    Phone VARCHAR(11) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE Mechanics --Мастера
+(
+    id SERIAL PRIMARY KEY,
+    firstname TEXT NOT NULL,
+    middlename TEXT,
+    lastname TEXT NOT NULL,
+    specialization VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE Zapchasti --Запчасти
+(
+    id SERIAL PRIMARY KEY,
+    zapchasti TEXT,
+    stoimost FLOAT
+);
+
+
+CREATE TABLE Orders --Заказы
+(
+    id SERIAL PRIMARY KEY,
+    tip_remonta TEXT,
+    custumer_id INT,
+    mechanic_id INT,
+    zapchasti_id INT,
+    itogovaya_stoimost FLOAT,
+    created_date Date,
+
+    FOREIGN KEY (custumer_id) REFERENCES Custumers(id)
+    FOREIGN KEY (master_id) REFERENCES Mechanics(id)
+    FOREIGN KEY (zapchasti_id) REFERENCES Zapchasti(id)
+);
